f6e0a17cab4ff1a5739591a136c5f26f406b00b3
Mingyu.Lee <leemingyu05@gmail.com>
2017-04-23 23:37:08 +0900

    Studying...

--- src/main/java/com/userfront/controller/Accountcontroller.java
+++ src/main/java/com/userfront/controller/Accountcontroller.java
@@ -1,7 +1,15 @@
 package com.userfront.controller;
 
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.SavingsAccount;
+import com.userfront.domain.User;
+import com.userfront.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 /**
  * Created by 이민규 on 2017-04-20.
@@ -10,13 +18,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class Accountcontroller {
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping("/primaryAccount")
-    public String primaryAccount() {
+    public String primaryAccount(Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+
         return "primaryAccount";
     }
 
     @RequestMapping("/savingsAccount")
-    public String savingsAccount() {
+    public String savingsAccount(Model model, Principal principal) {
+
+        User user = userService.findByUsername(principal.getName());
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("savingsAccount", savingsAccount);
+
         return "savingsAccount";
     }
 
